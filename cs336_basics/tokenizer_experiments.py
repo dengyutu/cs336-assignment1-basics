e265f7d5e0513cb67be28a2646a6bfabc3efe175
Dengyu Tu <dengyutu1@gmail.com>
2026-04-23 11:22:19 -0700

    Added the save_data function for both TinyStories and OpenWebText. Wrote the encode_by_boundary function for multiprocessing. Wrote the encode_iterable_by_boundary function for memory-eﬀicient tokenization of large files.

--- cs336_basics/tokenizer_experiments.py
+++ cs336_basics/tokenizer_experiments.py
@@ -2,8 +2,12 @@ import os
 import pathlib
 import random
 import time
+from collections.abc import Iterable, Iterator
+from multiprocessing import Pool
 
+import numpy as np
 from tokenizer import Tokenizer
+from train_bpe import find_chunk_boundaries
 
 
 def get_tokenizer(dataset_name: str) -> Tokenizer:
@@ -143,9 +147,114 @@ def tokenizer_throughput(dataset_name: str):
         print(f"it take to {calculate_time:.2f} seconds to tokenize the Pile dataset")
 
 
+def encode_by_boundary(input_path: str, start: int, end: int, tokenizer: Tokenizer):
+    with open(input_path, "rb") as f:
+        f.seek(start)
+        raw = f.read(end - start)
+        text = raw.decode("utf-8", errors="ignore")
+        tokens = tokenizer.encode(text)
+        tokens_array = np.array(tokens, dtype="uint16")
+    return tokens_array
+
+
+def text_generator(file: Iterable[bytes], start: int, end: int) -> Iterator[str]:
+    pos = start
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+    text = b""
+    while pos + mini_chunk_size < end:
+        file.seek(pos)
+        mini_chunk = file.read(mini_chunk_size)
+        found_at = mini_chunk.find(b"<|endoftext|>")
+        if found_at != -1:
+            text += mini_chunk[: found_at + len(b"<|endoftext|>")]
+            doc = text.decode("utf-8", errors="ignore")
+            # pos += found_at + len(b"<|endoftext|>")
+            yield doc
+            text = mini_chunk[found_at + len(b"<|endoftext|>") :]
+        else:
+            text += mini_chunk
+        pos += mini_chunk_size
+    file.seek(pos)
+    text += file.read(end - pos)
+    doc = text.decode("utf-8", errors="ignore")
+    yield doc
+
+
+def encode_iterable_by_boundary(input_path: str, start: int, end: int, tokenizer: Tokenizer):
+    with open(input_path, "rb") as f:
+        f.seek(start)
+        text_iterator = text_generator(f, start, end)
+        tokens = list(tokenizer.encode_iterable(text_iterator))
+        tokens_array = np.array(tokens, dtype="uint16")
+    return tokens_array
+
+
+def save_data_tinyStories(dataset_name: str):
+    training_data = (pathlib.Path(__file__).resolve().parent.parent) / "data/TinyStoriesV2-GPT4-train.txt"
+    valid_data = (pathlib.Path(__file__).resolve().parent.parent) / "data/TinyStoriesV2-GPT4-valid.txt"
+    num_processes = os.cpu_count()
+    tokenizer = get_tokenizer(dataset_name)
+    with open(training_data, "rb") as f:
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+        chunk_args = []
+        for start, end in zip(boundaries, boundaries[1:]):
+            chunk_args.append((training_data, start, end, tokenizer))
+        with Pool(processes=num_processes) as pool:
+            results = pool.starmap(encode_by_boundary, chunk_args)
+        all_tokens_array = np.concatenate(results)
+        np.save("TinyStoriesV2-GPT4-train_tokens", all_tokens_array)
+    with open(valid_data, "rb") as f:
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+        chunk_args = []
+        for start, end in zip(boundaries, boundaries[1:]):
+            chunk_args.append((valid_data, start, end, tokenizer))
+        with Pool(processes=num_processes) as pool:
+            results = pool.starmap(encode_by_boundary, chunk_args)
+        all_tokens_array = np.concatenate(results)
+        np.save("TinyStoriesV2-GPT4-valid_tokens", all_tokens_array)
+
+
+def save_data_OpenWebText(dataset_name: str):
+    training_data = (pathlib.Path(__file__).resolve().parent.parent) / "data/owt_train.txt"
+    valid_data = (pathlib.Path(__file__).resolve().parent.parent) / "data/owt_valid.txt"
+    num_processes = os.cpu_count()
+    tokenizer = get_tokenizer(dataset_name)
+    with open(training_data, "rb") as f:
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+        chunk_args = []
+        for start, end in zip(boundaries, boundaries[1:]):
+            chunk_args.append((training_data, start, end, tokenizer))
+        with Pool(processes=num_processes) as pool:
+            results = pool.starmap(encode_iterable_by_boundary, chunk_args)
+        all_tokens_array = np.concatenate(results)
+        np.save("owt_train_tokens", all_tokens_array)
+    with open(valid_data, "rb") as f:
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+        chunk_args = []
+        for start, end in zip(boundaries, boundaries[1:]):
+            chunk_args.append((valid_data, start, end, tokenizer))
+        with Pool(processes=num_processes) as pool:
+            results = pool.starmap(encode_by_boundary, chunk_args)
+        all_tokens_array = np.concatenate(results)
+        np.save("owt_valid_tokens", all_tokens_array)
+
+
 if __name__ == "__main__":
-    tokenizer_throughput("TinyStories")
-    tokenizer_throughput("OpenWebText")
+    # save_data_tinyStories("TinyStories")
+    save_data_OpenWebText("OpenWebText")
+
+    # this is to testing the correctness of the encode_iterable_by_boundary function.
+    # a = np.load("TinyStoriesV2-GPT4-valid_tokens.npy")
+    # b = np.load("TinyStoriesV2-GPT4-valid_tokens_2.npy")
+    # min_len = min(len(a), len(b))
+    # diffs = np.where(a[:min_len] != b[:min_len])[0]
+    # print(f"First diff at index: {diffs[0] if len(diffs) else 'none'}")
+    # print(f"a around diff: {a[diffs[0] - 5 : diffs[0] + 5]}")
+    # print(f"b around diff: {b[diffs[0] - 5 : diffs[0] + 5]}")
+    # assert np.array_equal(a, b), "Arrays differ"
+
+    # tokenizer_throughput("TinyStories")
+    # tokenizer_throughput("OpenWebText")
     # total_chunk_size = sample("TinyStories", 10)
     # total_chunk_size = sample("OpenWebText", 10)
     # cross_tokenize("TinyStories", 10)
